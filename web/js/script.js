20e6f40e0c0110180f1f0d2314b88e45e890e011
Saloni403 <156641454+Saloni403@users.noreply.github.com>
2026-01-13 17:48:53 +0530

    donee

--- web/js/script.js
+++ web/js/script.js
@@ -8,3 +8,16 @@ menuopen.addEventListener("click",()=>{
 menuclose.addEventListener("click",()=>{
     menu.style.left="-350px" 
 })
+
+let facebook = document.querySelector(".bi-facebook")
+facebook.addEventListener("click", ()=>{
+    window.open('','_blank')
+})
+let whatsapp = document.querySelector(".bi-whatsapp")
+whatsapp.addEventListener("click", ()=>{
+    window.open('','_blank')
+})
+let instagram = document.querySelector(".bi-instagram")
+instagram.addEventListener("click", ()=>{
+    window.open('','_blank')
+})
